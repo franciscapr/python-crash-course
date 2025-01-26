29e29a11a271c6e133952dd90186c5ac3026005b
Francisca <francisca.paredes.pr@gmail.com>
2025-01-26 12:40:30 -0300

    agregando nuevas pruebas

--- Cap11-Prueba_de_tu_codigo/test_name_function.py
+++ Cap11-Prueba_de_tu_codigo/test_name_function.py
@@ -5,3 +5,8 @@ def test_first_last_name():
     """¿Funcionan nombres como 'Janis Joplin?"""
     formatted_name = get_formatted_name('janis', 'joplin')
     assert formatted_name == 'Janis Joplin'
+
+def test_first_last_middle_name():
+    """¿Funcionan los nombres como 'Wolfgang Amadeus Mozart?"""
+    formatted_name = get_formatted_name('Wolfgang', 'mozart', 'amadeus')
+    assert formatted_name == 'Wolfgang Amadeus Mozart'
