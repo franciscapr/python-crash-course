b2b5ceba7af498d12497a19378de945d8a0adf2d
Francisca <francisca.paredes.pr@gmail.com>
2025-01-26 12:35:11 -0300

    respondiento una prueba fallida

--- Cap11-Prueba_de_tu_codigo/name_function.py
+++ Cap11-Prueba_de_tu_codigo/name_function.py
@@ -1,5 +1,23 @@
-def get_formatted_name(first, last):
-    """Genera un nombre completo con formato adecuado."""
-    full_name = f'{first} {last}'
+# funciòn con dos parametros
+
+#def get_formatted_name(first, last):
+ #   """Genera un nombre completo con formato adecuado."""
+ #   full_name = f'{first} {last}'
+ #   return full_name.title()
+
+# funciòn con error
+
+#def get_formatted_name(first, middle, last):
+#    """Genera un nombre completo formateado de manera ordenada."""
+#    full_name = f"{first} {middle} {last}"
+#    return full_name.title()
+
+# funciòn con tres parametros, middle es opcional
+
+def get_formatted_name(first, last, middle=''):
+    """Genera un nombre completo formateado de manera ordenada."""
+    if middle:
+        full_name = f"{first} {middle} {last}"
+    else:
+        full_name = f"{first} {last}"
     return full_name.title()
-    
